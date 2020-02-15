e6b754ac2196d2fb2de38335a4af17415ad3b86e
wilantury <ventas@antury.net>
2020-02-15 11:26:06 -0500

    Escribir la expresión regular para encontrara todas las letras minúsculas, ejecutar el comando .findall(file) para buscar en el archivo las coincidencias.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-# Resolve the problem!!
+import re
 
+# Resolve the problem!!
+p = re.compile('[a-z]')
 
 def run():
     # Start coding here
-
+    with open('src/encoded.txt', 'r', encoding='utf-8') as f:
+        text_list = p.findall(f.read())
+        text = ''.join(text_list)
+        print(text_list)
+        print(text)
 
 if __name__ == '__main__':
     run()
